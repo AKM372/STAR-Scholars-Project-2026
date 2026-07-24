62d3ea2ead353ac5721382f9d75469ae3c3fb148
AKM372 <akm372@drexel.edu>
2026-07-24 12:56:41 -0400

    Harder to clean dishes
    I made the area only detect the sponge some of the time. the plates were too easy to clean, i'm not sure if i like this better yet, but i want to keep it.

--- plate_script.gd
+++ plate_script.gd
@@ -85,7 +85,8 @@ func _on_fully_clean() -> void:
 
 #makes the dirt dissapear on sponge interaction!!
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if area.get_parent().is_in_group('sponge'):
+	
+	if area.get_parent().is_in_group('sponge') and randf() > 0.4:
 		sponge_touching = true
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
